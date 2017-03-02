79d53b86c4d6445475439dfd4e942d57b8f9de1b
Renato Golia <golia.renato@gmail.com>
2017-03-02 23:36:37 +0100

    ServiceCollectionExtensions
    Added shortcut to register IEventHandlers

--- Template/ServiceCollectionExtensions.cs
+++ Template/ServiceCollectionExtensions.cs
@@ -9,6 +9,16 @@ namespace Template
         {
             services.AddTransient<IRequestResponseHandler<TInput, TOutput>, THandler>();
             return services;
-        } 
+        }
+
+        public static IServiceCollection AddEventHandler<THandler, TInput>(this IServiceCollection services)
+            where THandler : class, IEventHandler<TInput>
+        {
+            services.AddTransient<IEventHandler<TInput>, THandler>();
+            return services;
+        }
     }
+
+
+
 }
