797c3fb297e0cb9758e7b0c7864875b1aa5c544d
Renato Golia <golia.renato@gmail.com>
2017-04-22 00:58:26 +0200

    Created SnsEventFunction sample

---
+++ samples/SnsEventFunction/Function.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Amazon.Lambda.Core;
+using Amazon.Lambda.Serialization;
+using Amazon.Lambda.SNSEvents;
+using Kralizek.Lambda;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace SnsEventFunction
+{
+    public class Function : EventFunction<SNSEvent>
+    {
+        protected override void Configure(IConfigurationBuilder builder)
+        {
+            builder.AddEnvironmentVariables();
+        }
+
+        protected override void ConfigureLogging(ILoggerFactory loggerFactory, IExecutionEnvironment executionEnvironment)
+        {
+            loggerFactory.AddLambdaLogger(new LambdaLoggerOptions
+            {
+                IncludeCategory = true,
+                IncludeLogLevel = true,
+                IncludeNewline = true
+            });
+        }
+
+        protected override void ConfigureServices(IServiceCollection services)
+        {
+            services.UseNotificationHandler<CustomNotification, CustomNotificationHandler>();
+        }
+    }
+
+    public class CustomNotification
+    {
+        public string Message { get; set; }
+    }
+
+    public class CustomNotificationHandler : INotificationHandler<CustomNotification>
+    {
+        private readonly ILogger<CustomNotificationHandler> _logger;
+
+        public CustomNotificationHandler(ILogger<CustomNotificationHandler> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public Task HandleAsync(CustomNotification notification, ILambdaContext context)
+        {
+            _logger.LogInformation($"Handling notification: {notification.Message}");
+
+            return Task.CompletedTask;
+        }
+    }
+}
