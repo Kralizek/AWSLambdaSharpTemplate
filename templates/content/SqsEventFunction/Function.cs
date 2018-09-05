1923260a8d4a5a5fcd19448de74d03908765ced7
Denys Zhuravel <denys.zhuravel@albelli.com>
2018-09-05 11:55:59 +0200

    copy paste sns function template to sqs function template

---
+++ templates/content/SqsEventFunction/Function.cs
@@ -0,0 +1,32 @@
+using Amazon.Lambda.Core;
+using Amazon.Lambda.SNSEvents;
+using Kralizek.Lambda;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace SnsEventFunction
+{
+    public class Function : EventFunction<SNSEvent>
+    {
+        protected override void Configure(IConfigurationBuilder builder)
+        {
+            // Use this method to register your configuration flow. Exactly like in ASP.NET Core
+        }
+
+        protected override void ConfigureLogging(ILoggerFactory loggerFactory, IExecutionEnvironment executionEnvironment)
+        {
+            // Use this method to install logger providers
+        }
+
+        protected override void ConfigureServices(IServiceCollection services)
+        {
+            // You need this line to register your handler
+            services.UseNotificationHandler<Notification, NotificationHandler>();
+
+            // Use this method to register your services. Exactly like in ASP.NET Core
+        }
+    }
+}
