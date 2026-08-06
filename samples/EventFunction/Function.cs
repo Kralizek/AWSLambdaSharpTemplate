cb1b84e2bb479387c8f2154ae7f767f97d0a77a2
Renato Golia <renato@golia.tech>
2026-08-07 00:24:26 +0200

    Align event sample with invocation context

--- samples/EventFunction/Function.cs
+++ samples/EventFunction/Function.cs
@@ -4,12 +4,10 @@ using System.Threading.Tasks;
 using Kralizek.Lambda;
 
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 using Amazon.Lambda.Core;
 
-// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
 namespace EventFunction;
@@ -23,8 +21,6 @@ public class Function : EventFunction<string, StringEventHandler>
 
     protected override void ConfigureLogging(ILoggingBuilder logging, IExecutionEnvironment executionEnvironment)
     {
-        logging.AddConfiguration(Configuration.GetSection("Logging"));
-
         logging.AddLambdaLogger(new LambdaLoggerOptions
         {
             IncludeCategory = true,
@@ -43,9 +39,9 @@ public class StringEventHandler : IEventHandler<string>
         _logger = logger;
     }
 
-    public ValueTask HandleAsync(string input, CancellationToken cancellationToken)
+    public ValueTask HandleAsync(string input, EventContext context, CancellationToken cancellationToken)
     {
-        _logger.LogInformation("Input: {Input}", input);
+        _logger.LogInformation("Input {Input} for request {AwsRequestId}", input, context.AwsRequestId);
         return ValueTask.CompletedTask;
     }
 }
