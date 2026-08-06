bdb8e7a05a132041fc7781cb15f8c7f64f7267ec
Renato Golia <renato@golia.tech>
2026-08-07 00:27:46 +0200

    Rename request sample to RequestFunction

---
+++ samples/RequestFunction/Function.cs
@@ -0,0 +1,47 @@
+using System.Threading;
+using System.Threading.Tasks;
+
+using Amazon.Lambda.Core;
+
+using Kralizek.Lambda;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace RequestFunction;
+
+public class Function : RequestFunction<string, string, UpperCaseHandler>
+{
+    protected override void Configure(IConfigurationBuilder builder)
+    {
+        builder.AddEnvironmentVariables();
+    }
+
+    protected override void ConfigureLogging(ILoggingBuilder logging, IExecutionEnvironment executionEnvironment)
+    {
+        logging.AddLambdaLogger(new LambdaLoggerOptions
+        {
+            IncludeCategory = true,
+            IncludeLogLevel = true,
+            IncludeNewline = true
+        });
+    }
+}
+
+public class UpperCaseHandler : IRequestHandler<string, string>
+{
+    private readonly ILogger<UpperCaseHandler> _logger;
+
+    public UpperCaseHandler(ILogger<UpperCaseHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public ValueTask<string> HandleAsync(string input, RequestContext context, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Input {Input} for request {AwsRequestId}", input, context.AwsRequestId);
+        return new ValueTask<string>(input.ToUpperInvariant());
+    }
+}
