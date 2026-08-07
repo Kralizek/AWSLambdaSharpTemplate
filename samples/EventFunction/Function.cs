3b10970a0f174e5e1db07d9749cc5cf979b81236
Renato Golia <renato@golia.tech>
2026-08-07 06:14:45 +0200

    Simplify event function sample

--- samples/EventFunction/Function.cs
+++ samples/EventFunction/Function.cs
@@ -5,31 +5,13 @@ using Amazon.Lambda.Core;
 
 using Kralizek.Lambda;
 
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
 namespace EventFunction;
 
-public class Function : EventFunction<string, StringEventHandler>
-{
-    protected override void ConfigureConfiguration(IConfigurationBuilder configuration)
-    {
-        configuration.AddEnvironmentVariables();
-    }
-
-    protected override void ConfigureLogging(ILoggingBuilder logging)
-    {
-        logging.AddConfiguration(Configuration.GetSection("Logging"));
-        logging.AddLambdaLogger(new LambdaLoggerOptions
-        {
-            IncludeCategory = true,
-            IncludeLogLevel = true,
-            IncludeNewline = true
-        });
-    }
-}
+public class Function : EventFunction<string, StringEventHandler>;
 
 public class StringEventHandler : IEventHandler<string>
 {
