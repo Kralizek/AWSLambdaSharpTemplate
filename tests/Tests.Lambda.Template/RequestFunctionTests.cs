7b3112ec93ce487def4c4024cb9efd8a0ae8c3d0
Renato Golia <renato@golia.tech>
2026-08-07 13:16:12 +0200

    Test strongly typed request contexts

--- tests/Tests.Lambda.Template/RequestFunctionTests.cs
+++ tests/Tests.Lambda.Template/RequestFunctionTests.cs
@@ -2,6 +2,8 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 
+using Amazon.Lambda.Core;
+
 using Kralizek.Lambda;
 
 using Microsoft.Extensions.Configuration;
@@ -55,6 +57,21 @@ public class RequestFunctionTests
     }
 
     [Test]
+    public async Task FunctionHandlerAsync_passes_specialized_context_without_casts()
+    {
+        SpecializedRequestHandler.ReceivedContext = null;
+        var sut = new SpecializedRequestFunction();
+        var lambdaContext = TestLambdaContexts.Create();
+
+        var result = await sut.FunctionHandlerAsync("hello", lambdaContext);
+
+        Assert.That(result, Is.EqualTo("hello:context"));
+        Assert.That(SpecializedRequestHandler.ReceivedContext, Is.Not.Null);
+        Assert.That(SpecializedRequestHandler.ReceivedContext?.Input, Is.EqualTo("hello"));
+        Assert.That(SpecializedRequestHandler.ReceivedContext?.LambdaContext, Is.SameAs(lambdaContext));
+    }
+
+    [Test]
     public void FunctionHandlerAsync_propagates_handler_exception()
     {
         var sut = new FailingHandlerFunction();
@@ -82,6 +99,22 @@ public class RequestFunctionTests
 
     public class FailingHandlerFunction : RequestFunction<string, string, ThrowingHandler> { }
 
+    public class SpecializedRequestFunction : RequestFunction<string, string, SpecializedRequestContext, SpecializedRequestHandler>
+    {
+        protected override SpecializedRequestContext CreateContext(string input, ILambdaContext context) => new(context, input);
+    }
+
+    public class SpecializedRequestContext : RequestContext
+    {
+        public SpecializedRequestContext(ILambdaContext context, string input)
+            : base(context)
+        {
+            Input = input;
+        }
+
+        public string Input { get; }
+    }
+
     public class NoOpHandler : IRequestHandler<string, string>
     {
         public ValueTask<string> HandleAsync(string input, RequestContext context, CancellationToken cancellationToken) => new(input);
@@ -102,6 +135,17 @@ public class RequestFunctionTests
         {
             ReceivedContext = context;
             return new ValueTask<string>(input + Suffix);
+        }
+    }
+
+    public class SpecializedRequestHandler : IRequestHandler<string, string, SpecializedRequestContext>
+    {
+        public static SpecializedRequestContext? ReceivedContext { get; set; }
+
+        public ValueTask<string> HandleAsync(string input, SpecializedRequestContext context, CancellationToken cancellationToken)
+        {
+            ReceivedContext = context;
+            return ValueTask.FromResult($"{input}:context");
         }
     }
 
