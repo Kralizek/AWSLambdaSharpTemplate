46e12a1da48f266bdc3ecd0fb97a2a516ad785fd
Renato Golia <renato@golia.tech>
2026-08-08 07:37:28 +0200

    Add raw SNS sample function

---
+++ samples/RawSnsFunction/Function.cs
@@ -0,0 +1,9 @@
+using Amazon.Lambda.Core;
+
+using Kralizek.Lambda;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace RawSnsFunction;
+
+public sealed class Function : SnsFunction<RawSnsRecordHandler>;
