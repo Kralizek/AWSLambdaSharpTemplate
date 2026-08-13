2150e0a594fc8b4b5665357678600a64bc214772
Renato Golia <renato@golia.tech>
2026-08-13 12:43:42 +0200

    Use payload serializer context in Native AOT sample

--- samples/NativeAotSqsFunction/Function.cs
+++ samples/NativeAotSqsFunction/Function.cs
@@ -6,7 +6,9 @@ namespace NativeAotSqsFunction;
 
 public sealed class Function : SqsFunction<OrderCreated, OrderCreatedHandler>
 {
-    protected override void ConfigurePayloadServices(IServiceCollection services) =>
+    protected override void ConfigurePayloadServices(IServiceCollection services)
+    {
         services.AddSingleton<IStringPayloadDecoder<OrderCreated>>(
-            new JsonStringPayloadDecoder<OrderCreated>(LambdaJsonSerializerContext.Default.OrderCreated));
+            new JsonStringPayloadDecoder<OrderCreated>(PayloadJsonSerializerContext.Default.OrderCreated));
+    }
 }
