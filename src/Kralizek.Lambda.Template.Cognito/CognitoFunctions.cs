92554ede529204e42d577f418c50337d770ad93e
Renato Golia <renato@golia.tech>
2026-08-13 14:35:34 +0200

    Propagate AOT handler metadata in Cognito

--- src/Kralizek.Lambda.Template.Cognito/CognitoFunctions.cs
+++ src/Kralizek.Lambda.Template.Cognito/CognitoFunctions.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 
 using Amazon.Lambda.CognitoEvents;
 using Amazon.Lambda.Core;
@@ -6,91 +7,91 @@ using Amazon.Lambda.Core;
 namespace Kralizek.Lambda;
 
 public interface ICognitoCreateAuthChallengeHandler : IRequestHandler<CognitoCreateAuthChallengeEvent, CognitoCreateAuthChallengeEvent>;
-public abstract class CognitoCreateAuthChallengeFunction<THandler> : RequestFunction<CognitoCreateAuthChallengeEvent, CognitoCreateAuthChallengeEvent, THandler>
+public abstract class CognitoCreateAuthChallengeFunction<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] THandler> : RequestFunction<CognitoCreateAuthChallengeEvent, CognitoCreateAuthChallengeEvent, THandler>
     where THandler : class, ICognitoCreateAuthChallengeHandler
 {
     protected override void EnrichInvocationActivity(Activity activity, CognitoCreateAuthChallengeEvent input, ILambdaContext context) => CognitoTelemetry.Enrich(activity, input);
 }
 
 public interface ICognitoCustomEmailSenderHandler : IRequestHandler<CognitoCustomEmailSenderEvent, CognitoCustomEmailSenderEvent>;
-public abstract class CognitoCustomEmailSenderFunction<THandler> : RequestFunction<CognitoCustomEmailSenderEvent, CognitoCustomEmailSenderEvent, THandler>
+public abstract class CognitoCustomEmailSenderFunction<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] THandler> : RequestFunction<CognitoCustomEmailSenderEvent, CognitoCustomEmailSenderEvent, THandler>
     where THandler : class, ICognitoCustomEmailSenderHandler
 {
     protected override void EnrichInvocationActivity(Activity activity, CognitoCustomEmailSenderEvent input, ILambdaContext context) => CognitoTelemetry.Enrich(activity, input);
 }
 
 public interface ICognitoCustomMessageHandler : IRequestHandler<CognitoCustomMessageEvent, CognitoCustomMessageEvent>;
-public abstract class CognitoCustomMessageFunction<THandler> : RequestFunction<CognitoCustomMessageEvent, CognitoCustomMessageEvent, THandler>
+public abstract class CognitoCustomMessageFunction<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] THandler> : RequestFunction<CognitoCustomMessageEvent, CognitoCustomMessageEvent, THandler>
     where THandler : class, ICognitoCustomMessageHandler
 {
     protected override void EnrichInvocationActivity(Activity activity, CognitoCustomMessageEvent input, ILambdaContext context) => CognitoTelemetry.Enrich(activity, input);
 }
 
 public interface ICognitoCustomSmsSenderHandler : IRequestHandler<CognitoCustomSmsSenderEvent, CognitoCustomSmsSenderEvent>;
-public abstract class CognitoCustomSmsSenderFunction<THandler> : RequestFunction<CognitoCustomSmsSenderEvent, CognitoCustomSmsSenderEvent, THandler>
+public abstract class CognitoCustomSmsSenderFunction<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] THandler> : RequestFunction<CognitoCustomSmsSenderEvent, CognitoCustomSmsSenderEvent, THandler>
     where THandler : class, ICognitoCustomSmsSenderHandler
 {
     protected override void EnrichInvocationActivity(Activity activity, CognitoCustomSmsSenderEvent input, ILambdaContext context) => CognitoTelemetry.Enrich(activity, input);
 }
 
 public interface ICognitoDefineAuthChallengeHandler : IRequestHandler<CognitoDefineAuthChallengeEvent, CognitoDefineAuthChallengeEvent>;
-public abstract class CognitoDefineAuthChallengeFunction<THandler> : RequestFunction<CognitoDefineAuthChallengeEvent, CognitoDefineAuthChallengeEvent, THandler>
+public abstract class CognitoDefineAuthChallengeFunction<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] THandler> : RequestFunction<CognitoDefineAuthChallengeEvent, CognitoDefineAuthChallengeEvent, THandler>
     where THandler : class, ICognitoDefineAuthChallengeHandler
 {
     protected override void EnrichInvocationActivity(Activity activity, CognitoDefineAuthChallengeEvent input, ILambdaContext context) => CognitoTelemetry.Enrich(activity, input);
 }
 
 public interface ICognitoUserMigrationHandler : IRequestHandler<CognitoMigrateUserEvent, CognitoMigrateUserEvent>;
-public abstract class CognitoUserMigrationFunction<THandler> : RequestFunction<CognitoMigrateUserEvent, CognitoMigrateUserEvent, THandler>
+public abstract class CognitoUserMigrationFunction<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] THandler> : RequestFunction<CognitoMigrateUserEvent, CognitoMigrateUserEvent, THandler>
     where THandler : class, ICognitoUserMigrationHandler
 {
     protected override void EnrichInvocationActivity(Activity activity, CognitoMigrateUserEvent input, ILambdaContext context) => CognitoTelemetry.Enrich(activity, input);
 }
 
 public interface ICognitoPostAuthenticationHandler : IRequestHandler<CognitoPostAuthenticationEvent, CognitoPostAuthenticationEvent>;
-public abstract class CognitoPostAuthenticationFunction<THandler> : RequestFunction<CognitoPostAuthenticationEvent, CognitoPostAuthenticationEvent, THandler>
+public abstract class CognitoPostAuthenticationFunction<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] THandler> : RequestFunction<CognitoPostAuthenticationEvent, CognitoPostAuthenticationEvent, THandler>
     where THandler : class, ICognitoPostAuthenticationHandler
 {
     protected override void EnrichInvocationActivity(Activity activity, CognitoPostAuthenticationEvent input, ILambdaContext context) => CognitoTelemetry.Enrich(activity, input);
 }
 
 public interface ICognitoPostConfirmationHandler : IRequestHandler<CognitoPostConfirmationEvent, CognitoPostConfirmationEvent>;
-public abstract class CognitoPostConfirmationFunction<THandler> : RequestFunction<CognitoPostConfirmationEvent, CognitoPostConfirmationEvent, THandler>
+public abstract class CognitoPostConfirmationFunction<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] THandler> : RequestFunction<CognitoPostConfirmationEvent, CognitoPostConfirmationEvent, THandler>
     where THandler : class, ICognitoPostConfirmationHandler
 {
     protected override void EnrichInvocationActivity(Activity activity, CognitoPostConfirmationEvent input, ILambdaContext context) => CognitoTelemetry.Enrich(activity, input);
 }
 
 public interface ICognitoPreAuthenticationHandler : IRequestHandler<CognitoPreAuthenticationEvent, CognitoPreAuthenticationEvent>;
-public abstract class CognitoPreAuthenticationFunction<THandler> : RequestFunction<CognitoPreAuthenticationEvent, CognitoPreAuthenticationEvent, THandler>
+public abstract class CognitoPreAuthenticationFunction<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] THandler> : RequestFunction<CognitoPreAuthenticationEvent, CognitoPreAuthenticationEvent, THandler>
     where THandler : class, ICognitoPreAuthenticationHandler
 {
     protected override void EnrichInvocationActivity(Activity activity, CognitoPreAuthenticationEvent input, ILambdaContext context) => CognitoTelemetry.Enrich(activity, input);
 }
 
 public interface ICognitoPreSignUpHandler : IRequestHandler<CognitoPreSignupEvent, CognitoPreSignupEvent>;
-public abstract class CognitoPreSignUpFunction<THandler> : RequestFunction<CognitoPreSignupEvent, CognitoPreSignupEvent, THandler>
+public abstract class CognitoPreSignUpFunction<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] THandler> : RequestFunction<CognitoPreSignupEvent, CognitoPreSignupEvent, THandler>
     where THandler : class, ICognitoPreSignUpHandler
 {
     protected override void EnrichInvocationActivity(Activity activity, CognitoPreSignupEvent input, ILambdaContext context) => CognitoTelemetry.Enrich(activity, input);
 }
 
 public interface ICognitoPreTokenGenerationHandler : IRequestHandler<CognitoPreTokenGenerationEvent, CognitoPreTokenGenerationEvent>;
-public abstract class CognitoPreTokenGenerationFunction<THandler> : RequestFunction<CognitoPreTokenGenerationEvent, CognitoPreTokenGenerationEvent, THandler>
+public abstract class CognitoPreTokenGenerationFunction<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] THandler> : RequestFunction<CognitoPreTokenGenerationEvent, CognitoPreTokenGenerationEvent, THandler>
     where THandler : class, ICognitoPreTokenGenerationHandler
 {
     protected override void EnrichInvocationActivity(Activity activity, CognitoPreTokenGenerationEvent input, ILambdaContext context) => CognitoTelemetry.Enrich(activity, input);
 }
 
 public interface ICognitoPreTokenGenerationV2Handler : IRequestHandler<CognitoPreTokenGenerationV2Event, CognitoPreTokenGenerationV2Event>;
-public abstract class CognitoPreTokenGenerationV2Function<THandler> : RequestFunction<CognitoPreTokenGenerationV2Event, CognitoPreTokenGenerationV2Event, THandler>
+public abstract class CognitoPreTokenGenerationV2Function<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] THandler> : RequestFunction<CognitoPreTokenGenerationV2Event, CognitoPreTokenGenerationV2Event, THandler>
     where THandler : class, ICognitoPreTokenGenerationV2Handler
 {
     protected override void EnrichInvocationActivity(Activity activity, CognitoPreTokenGenerationV2Event input, ILambdaContext context) => CognitoTelemetry.Enrich(activity, input);
 }
 
 public interface ICognitoVerifyAuthChallengeHandler : IRequestHandler<CognitoVerifyAuthChallengeEvent, CognitoVerifyAuthChallengeEvent>;
-public abstract class CognitoVerifyAuthChallengeFunction<THandler> : RequestFunction<CognitoVerifyAuthChallengeEvent, CognitoVerifyAuthChallengeEvent, THandler>
+public abstract class CognitoVerifyAuthChallengeFunction<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] THandler> : RequestFunction<CognitoVerifyAuthChallengeEvent, CognitoVerifyAuthChallengeEvent, THandler>
     where THandler : class, ICognitoVerifyAuthChallengeHandler
 {
     protected override void EnrichInvocationActivity(Activity activity, CognitoVerifyAuthChallengeEvent input, ILambdaContext context) => CognitoTelemetry.Enrich(activity, input);
