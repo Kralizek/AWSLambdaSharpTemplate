05982d7812dd600b351f5dbb7d7b85cee4f5184e
Renato Golia <renato@golia.tech>
2026-08-13 10:55:49 +0200

    Add Native AOT SQS sample

---
+++ samples/NativeAotSqsFunction/LambdaAot.cs
@@ -0,0 +1,27 @@
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+using Amazon.Lambda.RuntimeSupport;
+using Amazon.Lambda.Serialization.SystemTextJson;
+
+namespace NativeAotSqsFunction;
+
+internal static class LambdaAot
+{
+    private static async Task Main()
+    {
+        var function = new Function();
+        var serializer = new SourceGeneratorLambdaJsonSerializer<LambdaJsonSerializerContext>();
+
+        var bootstrap = LambdaBootstrapBuilder.Create<Amazon.Lambda.SQSEvents.SQSEvent, Amazon.Lambda.SQSEvents.SQSBatchResponse>(
+            function.FunctionHandlerAsync,
+            serializer);
+
+        await bootstrap.Build().RunAsync().ConfigureAwait(false);
+    }
+}
+
+[JsonSerializable(typeof(Amazon.Lambda.SQSEvents.SQSEvent))]
+[JsonSerializable(typeof(Amazon.Lambda.SQSEvents.SQSBatchResponse))]
+[JsonSerializable(typeof(OrderCreated))]
+internal partial class LambdaJsonSerializerContext : JsonSerializerContext;
