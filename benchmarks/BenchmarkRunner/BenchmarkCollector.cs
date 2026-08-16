ecc9602bc5c0d16ac662f8bddd883a1dd4aabde8
Renato Golia <renato@golia.tech>
2026-08-16 17:54:38 +0200

    Fix benchmark runner build imports

--- benchmarks/BenchmarkRunner/BenchmarkCollector.cs
+++ benchmarks/BenchmarkRunner/BenchmarkCollector.cs
@@ -1,7 +1,13 @@
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace BenchmarkRunner;
 
