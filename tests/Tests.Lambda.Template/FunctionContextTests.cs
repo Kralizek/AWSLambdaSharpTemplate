1badea84c8ace0bea3afe45efd11d8fc2f1ea5fb
Renato Golia <renato@golia.tech>
2026-08-17 12:16:33 +0200

    Test context property overlays

--- tests/Tests.Lambda.Template/FunctionContextTests.cs
+++ tests/Tests.Lambda.Template/FunctionContextTests.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 
 using Kralizek.Lambda;
 
@@ -63,6 +64,51 @@ public class FunctionContextTests
         });
     }
 
+    [Test]
+    public void Derived_context_overlay_exposes_new_property_and_preserves_existing_properties()
+    {
+        var lambdaContext = TestLambdaContexts.Create();
+        var metadata = FunctionContextFactory.CreateMetadata(lambdaContext);
+        var properties = FunctionContextFactory.CreateProperties(lambdaContext);
+        properties["Source"] = "SQS";
+
+        var source = new SpecializedRecordContext(metadata, properties);
+        var context = new OverlaidRecordContext(source, "Record", 42);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(context.Properties["Record"], Is.EqualTo(42));
+            Assert.That(context.Properties.TryGetValue("Record", out var record), Is.True);
+            Assert.That(record, Is.EqualTo(42));
+            Assert.That(context.Properties["Source"], Is.EqualTo("SQS"));
+            Assert.That(context.GetLambdaContext(), Is.SameAs(lambdaContext));
+        });
+    }
+
+    [Test]
+    public void Derived_context_overlay_replaces_existing_property_without_duplicating_it()
+    {
+        var lambdaContext = TestLambdaContexts.Create();
+        var metadata = FunctionContextFactory.CreateMetadata(lambdaContext);
+        var properties = FunctionContextFactory.CreateProperties(lambdaContext);
+        properties["Source"] = "SQS";
+        properties["Other"] = 42;
+
+        var source = new SpecializedRecordContext(metadata, properties);
+        var context = new OverlaidRecordContext(source, "Source", "Overlaid");
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(context.Properties.Count, Is.EqualTo(source.Properties.Count));
+            Assert.That(context.Properties["Source"], Is.EqualTo("Overlaid"));
+            Assert.That(context.Properties.TryGetValue("Source", out var sourceValue), Is.True);
+            Assert.That(sourceValue, Is.EqualTo("Overlaid"));
+            Assert.That(context.Properties["Other"], Is.EqualTo(42));
+            Assert.That(context.Properties.Keys.Count(key => key == "Source"), Is.EqualTo(1));
+            Assert.That(context.Properties.Count(pair => pair.Key == "Source"), Is.EqualTo(1));
+        });
+    }
+
     private sealed class SpecializedEventContext : EventContext
     {
         public SpecializedEventContext(
@@ -85,5 +131,14 @@ public class FunctionContextTests
             FunctionContextMetadata metadata,
             IReadOnlyDictionary<string, object?> properties)
             : base(metadata, properties) { }
+    }
+
+    private sealed class OverlaidRecordContext : RecordContext
+    {
+        public OverlaidRecordContext(
+            RecordContext source,
+            string propertyName,
+            object? propertyValue)
+            : base(source, propertyName, propertyValue) { }
     }
 }
