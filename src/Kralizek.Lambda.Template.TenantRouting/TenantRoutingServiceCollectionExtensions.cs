dd15d7e747ec465e5e608c827ef61b69aec1e594
Renato Golia <renato@golia.tech>
2026-09-22 14:06:42 +0200

    Add tenant routing runtime package

---
+++ src/Kralizek.Lambda.Template.TenantRouting/TenantRoutingServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+using Amazon.Lambda;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Kralizek.Lambda;
+
+public static class TenantRoutingServiceCollectionExtensions
+{
+    /// <summary>
+    /// Adds tenant-aware Lambda routing using the AWS Lambda SDK client.
+    /// </summary>
+    public static IServiceCollection AddTenantLambdaRouting(this IServiceCollection services)
+    {
+        services.TryAddSingleton<IAmazonLambda, AmazonLambdaClient>();
+        services.TryAddSingleton<ITenantLambdaRouter, TenantLambdaRouter>();
+
+        return services;
+    }
+}
